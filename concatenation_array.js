eafa5888a8a13e5aa24f8accda9b2b1a0eac4bfd
parag <paragjain528@gmail.com>
2022-09-15 19:51:59 +0530

    add: concatination program

---
+++ concatenation_array.js
@@ -0,0 +1,24 @@
+var getConcatenation = function(nums) {
+    var ans=[];
+    var n=nums.length;
+    for(var i=0;i<(2*n);i++)
+    {
+      ans[i]=nums[i%n];
+    }
+
+    return ans;
+};
+
+
+
+
+
+
+
+
+
+
+
+var nums=[1,4,6,];
+var ans=getConcatenation(nums);
+console.log(ans);
