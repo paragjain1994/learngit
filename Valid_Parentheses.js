4b1b38f9150ac30d8a5c2a01600123aac6c995ba
parag <paragjain528@gmail.com>
2022-09-15 22:25:15 +0530

    add:valid parentheses code

---
+++ Valid_Parentheses.js
@@ -0,0 +1,27 @@
+var isValid = (s) => {
+    var stack = [];
+    
+    for (let i = 0; i < s.length; i++) {
+    var top = stack[stack.length - 1];    //here length of stack array is dynamically changed according to need
+      if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+        stack.push(s[i]);
+      } else if (s[i] === ')' && top === '(' && stack.length !== 0) {
+        stack.pop();
+      } else if (s[i] === ']' && top === '[' && stack.length !== 0) {
+        stack.pop();
+      } else if (s[i] === '}' && top === '{' && stack.length !== 0) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+    if(stack.length === 0)
+    return true;
+    else return false;
+  };
+
+
+console.log(isValid('{[{}]'));
+
+
+
