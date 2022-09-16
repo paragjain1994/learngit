fd054a0b731d126213494e19286bbc9b7b0469fb
parag <paragjain528@gmail.com>
2022-09-17 00:11:16 +0530

    added:dom manipulation task

---
+++ dom.js
@@ -0,0 +1,28 @@
+//document.all[10].textContent="Item Lister has been changed to Hello";
+// console.log(document);
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[5]);
+// console.log(document.forms);
+// console.log(document.images);
+
+// var headerTitle=document.getElementById("header-title");
+// console.log(headerTitle);
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML='<h3>Hello world</h3>';
+// console.log(headerTitle.innerHTML);
+// headerTitle.style.borderBottom="solid 5px #000";
+//  var header=document.getElementById("main-header");
+//  console.log(header);
+//  header.style.border="solid 5px #000";
+
+ var additem=document.getElementsByClassName("title");
+ console.log(additem[0]);
+ additem[0].style.fontWeight='bold';
+ additem[0].style.color='green';
